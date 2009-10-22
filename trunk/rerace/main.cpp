fd073de1c299e0c19fe9e4ff318c84426d81ed25
drewag <drewag@0014a33e-bf56-11de-afc4-699a8c260df9>
2009-10-22 23:46:11 +0000

    Added OpenGL and GLUT Frameworks

--- trunk/rerace/main.cpp
+++ trunk/rerace/main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <GLUT/glut.h>
 
-int main (int argc, char * const argv[]) {
-    // insert code here...
-    std::cout << "YOU LIE\n";
+void display(void)
+{
+	/* clear window */
+	
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	/* draw unit square polygon */
+	glBegin(GL_POLYGON);
+	glColor3f(1.,0.,0.);
+	glVertex2f(-.05, -.05);
+	glVertex2f(-.05, .05);
+	glVertex2f(.05, .05);
+	glVertex2f(.05, -.05);
+	glEnd();
+	
+	/*  Empties all GL buffers, causing all issued commands to be executed
+	 as quickly as they are accepted by the actual rendering engine */
+	glFlush();
+}
+
+int main (int argc, char** argv) {
+    
+	/* Initialize mode and open a window in upper left corner of screen */
+	/* Window title is name of program (arg[0]) */
+	
+	glutInit(&argc, argv);
+	glutCreateWindow(argv[0]);
+	glutDisplayFunc(display);
+	glutMainLoop();
+	
     return 0;
 }
