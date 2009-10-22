caabf8bb06bb4d780b2236fc8f20b350695ccf41
thompk2 <thompk2@0014a33e-bf56-11de-afc4-699a8c260df9>
2009-10-22 22:56:57 +0000

    added Racer Class

---
+++ trunk/rerace/Racer.cpp
@@ -0,0 +1,11 @@
+/*
+ *  Racer.cpp
+ *  rerace
+ *
+ *  Created by Keith Thompson on 10/22/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "Racer.h"
+
---
+++ trunk/rerace/Racer.h
@@ -0,0 +1,9 @@
+/*
+ *  Racer.h
+ *  rerace
+ *
+ *  Created by Keith Thompson on 10/22/09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
