0329fe23c1664a88c8812fcd47ac48accb3ea140
drewag <drewag@0014a33e-bf56-11de-afc4-699a8c260df9>
2009-10-23 00:49:39 +0000

    Made Cube

--- rerace/main.cpp
+++ rerace/main.cpp
@@ -1,20 +1,68 @@
 #include <iostream>
 #include <GLUT/glut.h>
 
+void rect(GLfloat *va, GLfloat *vb, GLfloat *vc, GLfloat *vd)
+{
+	glVertex3fv(va);
+	glVertex3fv(vb);
+	glVertex3fv(vc);
+	glVertex3fv(vd);
+}
+
+void cube(GLfloat *point, GLfloat size)
+{
+	GLfloat *a = point;
+	GLfloat b[3] = {point[0] ,point[1], point[2]+size};
+	GLfloat c[3] = {point[0]+size,point[1], point[2]+size};
+	GLfloat d[3] = {point[0]+size ,point[1], point[2]};
+	
+	GLfloat e[3] = {point[0] ,point[1]+size, point[2]};
+	GLfloat f[3] = {point[0] ,point[1]+size, point[2]+size};
+	GLfloat g[3] = {point[0]+size ,point[1]+size, point[2]+size};
+	GLfloat h[3] = {point[0]+size ,point[1]+size, point[2]};
+	
+	GLfloat colors[6][3] = {{1.0, 0.0, 0.0}, {0.0, 1.0, 0.0},
+		{0.0, 0.0, 1.0}, {1.0, 1.0, 0.0}, {0.0, 1.0, 1.0}, {1.0, 0.0, 1.0}};
+	
+	// Top
+    glColor3fv(colors[0]);
+    rect(a, b, c, d);
+	
+	// Left
+    glColor3fv(colors[1]);
+    rect(a, b, f, e);
+	
+	// Right
+    glColor3fv(colors[2]);
+    rect(d, c, g, h);
+	
+	// Bottom
+    glColor3fv(colors[3]);
+    rect(e, f, g, h);
+	
+	// Back
+    glColor3fv(colors[4]);
+    rect(a, d, h, e);
+	
+	// Front
+    glColor3fv(colors[5]);
+    rect(b, c, g, f);
+}
+
 void display(void)
 {
 	/* clear window */
-	
+	glRotatef(5, 0, 1, 0);
+	glRotatef(5, 0, 0, 1);
+	glRotatef(5, 1, 0, 0);
 	
 	glClear(GL_COLOR_BUFFER_BIT);
 	
 	/* draw unit square polygon */
 	glBegin(GL_POLYGON);
-	glColor3f(1.,0.,0.);
-	glVertex3f(-.05, -.05, -.5);
-	glVertex3f(-.05, .05);
-	glVertex3f(.05, .05);
-	glVertex3f(.05, -.05);
+	GLfloat startPoint[3] = {-.8, -.8, -.8};
+	GLfloat startSize = 1.6;
+	cube(startPoint, startSize);
 	glEnd();
 	
 	/*  Empties all GL buffers, causing all issued commands to be executed
